ccb2e8e2659f0e579014733aa479fa05d0229497
jagennivasan <professorjagan18@gmail.com>
2024-07-18 01:06:17 +0530

    adding routing links for login & signup

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeComponent from "./pages/HomeComponent/HomeComponent";
 import Header from "./components/layout/Header";
+import Login from "./components/auth/Login";
+import SignUp from "./components/auth/SignUp";
 const App = () => {
   return (
     <BrowserRouter>
     <Header/>
       <Routes>
         <Route path="/" element={<HomeComponent/>} />
+        <Route path="login" element={<Login/>}/>
+        <Route path="signup" element={<SignUp/>}/>
+
       </Routes>
     </BrowserRouter>
   );
